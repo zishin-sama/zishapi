c02e6ae447f14d39987ef48798231ddb0701379b
zishin-sama <rydextrex@gmail.com>
2024-10-30 18:47:09 +0800

    update tempmail.js

--- api/tempmail.js
+++ api/tempmail.js
@@ -3,7 +3,7 @@ const { TempMail } = require('1secmail-api');
 exports.config = {
 	name: "tempmail",
 	author: 'Zishin Sama',
-    description: 'Get ',
-    category: 'ai',
-    usage: ['/gpt?q=hi&id=1']
+    description: 'Get temporary email (auto get inbox)',
+    category: 'tools',
+    usage: ['/tempmail']
 }
