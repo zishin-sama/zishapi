e5d50afa969197c19173157b542b1f8d72584ba0
zishin-sama <rydextrex@gmail.com>
2024-10-31 09:52:02 +0800

    update cookenGetter.js

--- api/cookenGetter.js
+++ api/cookenGetter.js
@@ -17,12 +17,25 @@ async function getUserCookie(email, password) {
         const session = axios.create({
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Linux; Android 4.1.2; GT-I8552 Build/JZO54K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-language': 'en_US',
+      'cache-control': 'max-age=0',
+      'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': "Windows",
+      'sec-fetch-dest': 'document',
+      'sec-fetch-mode': 'navigate',
+      'sec-fetch-site': 'same-origin',
+      'sec-fetch-user': '?1',
+      'upgrade-insecure-requests': '1',
+      'user-agent': user_agent
             }
         });
 
         const response = await session.get(url);
         const $ = cheerio.load(response.data);
+        
+        const insp
 
         const data = {
             lsd: $('input[name="lsd"]').val(),
@@ -101,4 +114,4 @@ exports.initialize = async ({req, res}) => {
         res.setHeader('Content-Type', 'application/json');
         return res.status(500).send(JSON.stringify({ error: 'Internal Server Error' }, null, 2));
     }
-};
+}
